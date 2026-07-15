ef3956e7f685c6e31ce160fba2ac3b0fd8959970
Erik Bogado <erikbogado@gmail.com>
2026-07-15 17:13:23 -0300

    revert: restore automatic IPv6 DNS

--- unifi/modules/network/variables.tf
+++ unifi/modules/network/variables.tf
@@ -1,20 +1,18 @@
 variable "networks" {
   description = "VLANs / networks, keyed by name."
   type = map(object({
-    purpose          = string           # corporate | guest | wan | vlan-only
-    subnet           = optional(string) # CIDR, e.g. "192.168.10.1/24"
-    vlan_id          = optional(number)
-    network_group    = optional(string, "LAN")
-    multicast_dns    = optional(bool)
-    dhcp_enabled     = optional(bool, true)
-    dhcp_start       = optional(string)
-    dhcp_stop        = optional(string)
-    dhcp_lease       = optional(number)
-    dhcp_dns         = optional(list(string))
-    dhcp_v6_dns_auto = optional(bool)
-    dhcp_v6_dns      = optional(list(string))
-    domain_name      = optional(string)
-    igmp_snooping    = optional(bool)
+    purpose       = string           # corporate | guest | wan | vlan-only
+    subnet        = optional(string) # CIDR, e.g. "192.168.10.1/24"
+    vlan_id       = optional(number)
+    network_group = optional(string, "LAN")
+    multicast_dns = optional(bool)
+    dhcp_enabled  = optional(bool, true)
+    dhcp_start    = optional(string)
+    dhcp_stop     = optional(string)
+    dhcp_lease    = optional(number)
+    dhcp_dns      = optional(list(string))
+    domain_name   = optional(string)
+    igmp_snooping = optional(bool)
   }))
   default = {}
 }
