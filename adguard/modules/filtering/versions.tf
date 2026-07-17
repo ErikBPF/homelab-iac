369f0d546415e436f24b0a961317e010af0b6961
Erik Bogado <erikbogado@gmail.com>
2026-07-16 21:17:17 -0300

    fix(adguard): pin provider 0.1.8

--- adguard/modules/filtering/versions.tf
+++ adguard/modules/filtering/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     adguardhome = {
       source  = "registry.terraform.io/ErikBPF/adguardhome"
-      version = "= 0.1.7"
+      version = "= 0.1.8"
     }
   }
 }
