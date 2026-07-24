372b6cfa80720c6dbc1977460f1aa7fcc93e88c5
Erik Bogado <erikbogado@gmail.com>
2026-07-23 23:35:29 -0300

    fix(openbao): avoid reserved version input

--- components/openbao/modules/kv-secret/main.tf
+++ components/openbao/modules/kv-secret/main.tf
@@ -2,5 +2,5 @@ resource "vault_kv_secret_v2" "this" {
   mount                = var.mount
   name                 = var.name
   data_json_wo         = jsonencode(var.data)
-  data_json_wo_version = var.version
+  data_json_wo_version = var.write_version
 }
