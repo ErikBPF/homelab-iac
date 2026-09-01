1f4aa456cab3fd6f4f646c6badd838c330f993c6
Erik Bogado <erikbogado@gmail.com>
2026-09-01 17:24:40 -0300

    fix(harbor): cover future IAM projects (#92)

--- components/harbor-iam/modules/oidc/variables.tf
+++ components/harbor-iam/modules/oidc/variables.tf
@@ -7,8 +7,3 @@ variable "harbor_project_iam_manager_secret_version" {
   type    = number
   default = 2
 }
-
-variable "harbor_project_iam_manager_projects" {
-  type    = set(string)
-  default = ["dockerhub", "ghcr", "k8s", "langfuse", "library", "lscr", "quay", "risingwave"]
-}
